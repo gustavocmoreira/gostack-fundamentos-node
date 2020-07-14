0ccdbf01e8932da6b896246630ebfc808b88cd94
Gustavo Moreira <gustavoclebermoreira@gmail.com>
2020-07-13 22:45:57 -0300

    Criação da funcionalidade de listar transactions e criar transactions

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,15 +1,35 @@
 import { Router } from 'express';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+ import TransactionsRepository from '../repositories/TransactionsRepository';
+ import CreateTransactionService from '../services/CreateTransactionService';
+import Transaction from '../models/Transaction';
 
 const transactionRouter = Router();
 
-// const transactionsRepository = new TransactionsRepository();
+const transactionsRepository = new TransactionsRepository();
 
+interface Balance {
+  income: number;
+  outcome: number;
+  total: number;
+}
+
+interface Report{
+  transactions: Transaction[];
+  balance:Balance
+}
 transactionRouter.get('/', (request, response) => {
   try {
-    // TODO
+    const transactions = transactionsRepository.all();
+    const balance = transactionsRepository.getBalance();
+    const report: Report = {
+      transactions,
+      balance
+    };
+
+
+    return response.json(report);
+
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
@@ -17,6 +37,18 @@ transactionRouter.get('/', (request, response) => {
 
 transactionRouter.post('/', (request, response) => {
   try {
+   const {title,value,type} = request.body;
+
+   const createTransaction = new CreateTransactionService(transactionsRepository);
+
+   const transaction = createTransaction.execute({
+     title,
+     value,
+     type
+   });
+
+   return response.json(transaction)
+
     // TODO
   } catch (err) {
     return response.status(400).json({ error: err.message });
